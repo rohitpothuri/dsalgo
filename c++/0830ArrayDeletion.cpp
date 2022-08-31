4816ed9a30c068bdb87157a61614057d1e29c2f5
Rohit Pothuri <rohitpothuri@Rohits-MacBook-Pro.local>
2022-08-30 21:19:42 -0400

    linear search 2

--- c++/0830ArrayDeletion.cpp
+++ c++/0830ArrayDeletion.cpp
@@ -6,23 +6,22 @@ int main(){
     char str;
     cin>>str;
     char colors[7] = {'V','I','B','G','Y','O','R'};
+
     int found = 0;
     for(int i=0;i<7;i++){
       if(str == colors[i])
         found = 1;
+    }
+    if(found==0){
+      cout << "Color not available";
+      return 0;
     }
 
     if(!isupper(str)){
       cout << "Input character should be uppercased";
       return 0;
     }
-
-    if(found==0){
-      cout << "Color not available";
-      return 0;
-    }
-      
-
+    
     for(int i=0;i<7;i++){
       if(str != colors[i])
         cout << colors[i];
