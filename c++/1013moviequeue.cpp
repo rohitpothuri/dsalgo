5e56f79c1d4f374744e777aca38b22312aa13757
Rohit Pothuri <rohitpothuri@Rohits-MacBook-Pro.local>
2022-10-13 18:33:27 -0400

    1013 movie queue

---
+++ c++/1013moviequeue.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    queue <string> v;
+    int seats;
+    cin >> seats;
+    int people;
+    cin >> people;
+
+    for(int i=0; i<seats; i++){
+        string name;
+        cin >> name;
+        v.push(name);
+    }
+
+    if(people>seats){
+        cout << "Housefull";
+        return 0;
+    }
+    else{
+        cout << "Booked";
+        return 0;
+    }
+        
+}
