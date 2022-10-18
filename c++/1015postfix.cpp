05e42640c155bb8b32144f43dd2e9297288f2ce0
Rohit Pothuri <rohitpothuri@Rohits-MacBook-Pro.local>
2022-10-17 20:28:19 -0400

    1017 ll reverse

--- c++/1015postfix.cpp
+++ c++/1015postfix.cpp
@@ -1,103 +1,59 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <vector>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-// Converts string s to a int.
-int string_to_double(const string& s) {
-  return stoi(s);
-}
-
-string token;          // global variable
-vector<string> stack;  // global variable
-const string prompt = "--> ";
-
-// remove and return the element of the stack
-int pop() {
-    if (stack.size() == 0) {
-        cout << "tried to pop an empty stack"; // throws an exception
-        return 0;  // never called: only needed to satisfy the compiler
-    } else {
-        int tos = stoi(stack.back());
-        stack.pop_back();
-        return tos;
+bool val(string a){
+    int n = a.length(), br = 0;
+    for(int i = 0; i < n; i++){
+        if(isdigit(a[i])){
+            br++;
+        }
+        else{
+            br -= 2;
+            if(br < 0){
+                return false;
+            }
+            br++;
+        }
     }
+    if(br == 1)
+        return true;
+    return false;
 }
 
-// put x on the top of the stack
-void push(int x) {
-    stack.push_back(std::to_string(x));
-}
-
-int main() {
-    
-    int i = 0;
-    std::string token;
-    string previousOp = "";
-    bool test = false;
-
-
-    while (getline(std::cin, token)) {
-        if((previousOp == "+") | (previousOp == "*") | (previousOp == "-") | (previousOp == "/")){
-        test = true;
-        }
-         else test = false;
-        if (token == "+" & !test) {
-            try {
-                previousOp = token;
-                int b = pop();
-                int a = pop();
-                push(a + b);
-            } catch (runtime_error e) {
-                cout << "Error: stack is empty\n";
-                cout << prompt;
-            }
-        } else if (token == "-" & !test) {
-            try {
-                previousOp = token;
-                int b = pop();
-                int a = pop();
-                push(a - b);
-            } catch (runtime_error e) {
-                cout << "Error: stack is empty\n";
-                cout << prompt;
-            }
-        } else if (token == "*" & !test) {
-            try {
-                previousOp = token;
-                int b = pop();
-                int a = pop();
-                push(a * b);
-            } catch (runtime_error e) {
-                cout << "Error: stack is empty\n";
-                cout << prompt;
-            }
-        } else if (token == "/" & !test) {
-            try {
-                previousOp = token;
-                int b = pop();
-                int a = pop();
-                push(a / b);
-            } catch (runtime_error e) {
-                cout << "Error: stack is empty\n";
-                cout << prompt;
-            }
-        } else if (token.empty()) {
-                break;
-        }
-        else if(((token == "+") | (token == "*") | (token == "-") | (token == "/")) & test){
-                push(-100);
-                break;
-        }
-         else {
-            previousOp = token;
-            push(string_to_double(token));
-        }
-        i++;
+int main(){
+    string q;
+    stack<int> h;
+    for(int i=0;i<100;i++){
+        string b;
+        cin >> b;
+        q.append(b);
     }
-    string last = stack.back();
-    cout << last << "\n";
+    if(val(q)){
+        for(int i=0; i < q.length(); i++){
+
+            if (isdigit(q[i]))
+                h.push(q[i] - '0');
+
+            else{
+                int op2=h.top();
+                h.pop();
+                int op1=h.top();
+                h.pop();
+
+                if(q[i] == '+')
+                    h.push(op1 + op2);
+                else if(q[i] == '-')
+                    h.push(op1 - op2);
+                else if(q[i] == '*')
+                    h.push(op1 * op2);
+                else if(q[i] == '/')
+                    h.push(op1 / op2);
+            }
+        }
+        cout << h.top();
+    }
+    else{
+        cout<<"Invalid Input";
+    }
     return 0;
 }
